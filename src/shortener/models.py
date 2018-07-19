6a7cdbcb861a4b421b951cdd19bb1f27ab88db7a
iamnadeem <nadeem.a9891@gmail.com>
2018-07-19 11:45:09 +0530

    video 13 commit

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -1,12 +1,13 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 
 class KirrURL(models.Model):
 	url 		= models.CharField(max_length=220, )
 	shortcode 	= models.CharField(max_length=15, unique=True)
-	updated 	= models.DateTimeField(auto_now=True) 	#everytime the model is saved
-	timestamp	= models.DateTimeField(auto_now_add=True)	#when model was created
+	updated 	= models.DateTimeField(auto_now=True, ) 	#everytime the model is saved
+	timestamp	= models.DateTimeField(auto_now_add=True,)	#when model was created
 	#empty_datetime = models.DateTimeField(auto_now=False, auto_now_add=False)
 	#shortcode = models.CharField(max_length=15, null=True) Empty in database is okay
 	#shortcode = models.CharField(max_length=15, default='cfedefaultshortcode')
