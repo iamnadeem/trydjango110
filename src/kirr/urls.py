74b0b936a769de1e2eef0ff2642dae6cd827f5e1
iamnadeem <nadeem.a9891@gmail.com>
2018-07-22 22:58:24 +0530

    Video 22 commit

--- src/kirr/urls.py
+++ src/kirr/urls.py
@@ -21,6 +21,6 @@ from shortener.views import kirr_redirect_view, KirrCBView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^a/(?P<shortcode>[\w-]+)/$', kirr_redirect_view),
-    url(r'^b/(?P<shortcode>[\w-]+)/$', KirrCBView.as_view()),  #join.cfe.com/projects/ python regex
+    url(r'^a/(?P<shortcode>[\w-]+){6,15}/$', kirr_redirect_view),
+    url(r'^b/(?P<shortcode>[\w-]+){6,15}/$', KirrCBView.as_view()),  #join.cfe.com/projects/ python regex
 ]
