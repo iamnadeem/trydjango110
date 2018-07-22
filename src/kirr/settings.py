801ee36b9a81d16d6d68e95cc0c696d66bfc6315
iamnadeem <nadeem.a9891@gmail.com>
2018-07-22 14:12:44 +0530

    Video 19 commit

--- src/kirr/settings.py
+++ src/kirr/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     #custom app
-    'shortener',
+    'shortener', 
 ]
 
 MIDDLEWARE = [
@@ -121,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+SHORTCODE_MAX = 10
+SHORTCODE_MIN = 4
--- src/shortener/models.py
+++ src/shortener/models.py
@@ -1,7 +1,11 @@
+from django.conf import settings
 from django.db import models
 from datetime import datetime
 
+
 from .utils import code_generator, create_shortcode
+
+SHORTCODE_MAX = getattr(settings, "SHORTCODE_MAX", 15)
 
 # Create your models here.
 
@@ -27,7 +31,7 @@ class KirrURLManager(models.Manager):
 
 class KirrURL(models.Model):
 	url 		= models.CharField(max_length=220, )
-	shortcode 	= models.CharField(max_length=15, unique=True, blank=True)
+	shortcode 	= models.CharField(max_length=SHORTCODE_MAX, unique=True, blank=True)
 	updated 	= models.DateTimeField(auto_now=True, ) 	#everytime the model is saved
 	timestamp	= models.DateTimeField(auto_now_add=True,)	#when model was created
 	active		= models.BooleanField(default=True)
