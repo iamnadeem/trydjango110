3666e3bd9694c0cc345582f462a0951091e246e5
iamnadeem <nadeem.a9891@gmail.com>
2018-07-22 13:28:09 +0530

    video 18 commit

--- src/shortener/views.py
+++ src/shortener/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+def kirr_redirect_view(request, *args, **kwargs):  #function based view FBV
+	return HttpResponse("hello")
+
+
+class KirrCBView(View):  #class based view  CBV
+	def get(self, request, *agrs, **kwargs):
+		return HttpResponse("hello again")
+	
+		
