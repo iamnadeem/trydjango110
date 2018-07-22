bfff44ef44320bfead49dde04d88dfcd92d44280
iamnadeem <nadeem.a9891@gmail.com>
2018-07-22 22:44:38 +0530

    Video 21 commit

--- src/kirr/urls.py
+++ src/kirr/urls.py
@@ -20,7 +20,7 @@ from django.contrib import admin
 from shortener.views import kirr_redirect_view, KirrCBView
 
 urlpatterns = [
-    url(r'^new-admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
     url(r'^a/(?P<shortcode>[\w-]+)/$', kirr_redirect_view),
     url(r'^b/(?P<shortcode>[\w-]+)/$', KirrCBView.as_view()),  #join.cfe.com/projects/ python regex
 ]
--- src/shortener/views.py
+++ src/shortener/views.py
@@ -1,16 +1,43 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
+
+from .models import KirrURL
 
 # Create your views here.
 def kirr_redirect_view(request, shortcode=None ,*args, **kwargs):  #function based view FBV
-	# print(request.user)
-	# print(request.user.is_authenticated())
-	return HttpResponse("hello {sc}".format(sc=shortcode))
+	obj = get_object_or_404(KirrURL, shortcode=shortcode)
+	return HttpResponse("hello {sc}".format(sc=obj.url))
 
 
 class KirrCBView(View):  #class based view  CBV
 	def get(self, request, shortcode=None ,*args, **kwargs):
+		obj = get_object_or_404(KirrURL, shortcode=shortcode)
 		return HttpResponse("hello again {sc}".format(sc=shortcode))
 	
+	def post(self, request, *args, **kwargs):
+		return HttpResponse()
+
+
+
+
+
+'''
+# Create your views here.
+def kirr_redirect_view(request, shortcode=None ,*args, **kwargs):  #function based view FBV
+	# print(request.user)
+	# print(request.user.is_authenticated())
+	#obj = KirrURL.objects.get(shortcode=shortcode)
+
+	obj = get_object_or_404(KirrURL, shortcode=shortcode)
+	# obj_url = obj.url
+
+
+	# try:
+	# 	obj = KirrURL.objects.get(shortcode=shortcode)
+	# except:
+	# 	obj = KirrURL.objects.all().first()
+
+	return HttpResponse("hello {sc}".format(sc=obj.url))
+'''
 		
