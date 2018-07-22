014144b7f5c1706f78986aa370cec0a0529f13f0
iamnadeem <nadeem.a9891@gmail.com>
2018-07-23 00:26:01 +0530

    Video 24 commit

---
+++ src/kirr/hosts.py
@@ -0,0 +1,18 @@
+from django.conf import settings
+from django_hosts import patterns, host
+
+host_patterns = patterns('',
+    host(r'www', settings.ROOT_URLCONF, name='www'),
+    host(r'(?!www).*', 'kirr.hostsconf.urls', name='wildcard'),
+)
+
+
+'''
+#old method
+from kirr.hostsconf import urls as redirect_urls
+host_patterns = patterns('',
+    host(r'www', settings.ROOT_URLCONF, name='www'),
+    host(r'(?!www).*', redirect_urls, name='wildcard'),
+
+
+'''
