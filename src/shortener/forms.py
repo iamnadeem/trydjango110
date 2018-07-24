37d54e579d2a15c5ba3ab6d4cab07b5dc5a3175a
iamnadeem <nadeem.a9891@gmail.com>
2018-07-24 17:54:48 +0530

    Video 32 commit

--- src/shortener/forms.py
+++ src/shortener/forms.py
@@ -4,7 +4,16 @@ from .validators import validate_url, validate_dot_com
 
 
 class SubmitUrlForm(forms.Form):
-	url = forms.CharField(label='Submit URL', validators=[validate_url, validate_dot_com])
+	url = forms.CharField(
+		label='', 
+		validators=[validate_url],
+		widget = forms.TextInput(
+				attrs={
+					"placeholder": "Long URL",
+					"class": "form-control"
+				}
+			)
+		)
 	# abc = url = forms.CharField(label='Submit URL')
 
 	# def clean(self):
